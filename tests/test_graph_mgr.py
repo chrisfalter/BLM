05f58f54b6fa3759426c7cb8824d42925075208a
Christopher Falter <cfalter@iu.edu>
2021-05-29 14:05:41 -0400

    add capability of save/restore graph

--- tests/test_graph_mgr.py
+++ tests/test_graph_mgr.py
@@ -1,3 +1,4 @@
+import os
 from sys import stderr
 
 import pytest
@@ -15,17 +16,37 @@ def get_graph_mgr():
     return urg    
 
 
+@pytest.fixture
+def get_temp_file():
+    fname = "temp.ncol"
+    yield fname
+    os.remove(fname)
+
+    
 def test_graph_isDirectedWeighted(get_graph_mgr):
     urg = get_graph_mgr
-    assert urg.g.is_weighted()
-    assert urg.g.is_directed()
-    assert urg.g.is_named()
     assert urg.edge_weights[("A","B")] == 2
-    assert urg.g.strength(["A", "B", "C", "F"], "IN", weights="weight") == [0,2,0,1]
-    assert urg.g.strength(["A", "B", "C", "F"], "OUT", weights="weight") == [2,0,1,0]
+    verify_all_graph_properties(urg.g)
 
 
+def verify_all_graph_properties(g):
+    assert g.is_weighted()
+    assert g.is_directed()
+    assert g.is_named()
+    assert g.strength(["A", "B", "C", "F"], "IN", weights="weight") == [0,2,0,1]
+    assert g.strength(["A", "B", "C", "F"], "OUT", weights="weight") == [2,0,1,0]
+
+    
 def test_influenceScores_areSortedCorrectly(get_graph_mgr):
     urg: UserRetweetGraph = get_graph_mgr
     influence_scores = urg.get_influence_scores()
     assert influence_scores["B"] > influence_scores["A"]
+
+
+def test_load_rehydratesAllVerticesAndWeights(get_graph_mgr, get_temp_file):
+    urg: UserRetweetGraph = get_graph_mgr
+    tf = get_temp_file
+    urg.save_graph(tf)
+    urg2 = UserRetweetGraph()
+    urg2.load_graph(tf)
+    verify_all_graph_properties(urg2.g)
